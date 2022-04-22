b10ad026639b84598089d4e6e375a34ae29d829d
Vikas <vikasreddy846@gmail.com>
2022-04-22 02:56:47 -0500

    Button to invite or add friend

--- client/src/AddFriendButton.js
+++ client/src/AddFriendButton.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
+import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+import AddFriendDialog from "./AddFriendDialog";
 
-import Button1 from './Button1';
-const additionalStyles={
-    marginTop: '10px',
-    marginleft: '5px',
-    width: '80%',
-    height: '30px',
-    background: '#3ba55d'
-}
-const AddFriendButton=()=>{
-    return(
-        <div>
-            <Button1
-            additionalStyles={additionalStyles}
-            label='AddFriend'
-            />
-
-        </div>
-    );
+const additionalStyles = {
+  marginTop: "10px",
+  marginLeft: "5px",
+  width: "80%",
+  height: "30px",
+  background: "#3ba55d",
 };
+
+const AddFriendButton = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleOpenAddFriendDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleCloseAddFriendDialog = () => {
+    setIsDialogOpen(false);
+  };
+
+  return (
+    <>
+      <CustomPrimaryButton
+        additionalStyles={additionalStyles}
+        label="Add Friend"
+        onClick={handleOpenAddFriendDialog}
+      />
+      <AddFriendDialog
+        isDialogOpen={isDialogOpen}
+        closeDialogHandler={handleCloseAddFriendDialog}
+      />
+    </>
+  );
+};
+
 export default AddFriendButton;
