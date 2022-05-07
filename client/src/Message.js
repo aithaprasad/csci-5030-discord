8c64e83dd285f669edd12a99edfbdc64cf3e53b3
aithaprasad <aitha.prasad@bennett.edu.in>
2022-05-06 22:43:50 -0500

    Added auto-scroll to the latest message, added emoji feature(we get a popup to chose a emoji and send it), Improvised Connected the API's for handling the direct chat with friend and handled the new connection when a user accepts an invite from other person

--- client/src/Message.js
+++ client/src/Message.js
@@ -1,14 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../Message.css";
+import { sendDirectMessage } from "../../realtimeCommunication/socketConnection";
+import InputEmoji from "react-input-emoji";
 
 function Message({ timestamp, message, user }) {
+  const messagesEndRef = useRef(null);
   const [userName, setUserName] = useState("Uday");
+  const [inputMessage, setInputMessage] = useState("");
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
   useEffect(() => {
     console.log(localStorage.getItem("jwt"));
     console.log(localStorage.getItem("user"));
     const localUser = JSON.parse(localStorage.getItem("user"));
     setUserName(localUser["name"]);
+    scrollToBottom();
   });
+  const handleMessageValueChange = (event) => {
+    if (event === "") {
+      return;
+    }
+    setInputMessage(event.target.value);
+  };
+
+  const handleKeyPressed = (event) => {
+    if (event.key === "Enter") {
+      handleSendMessage();
+    }
+  };
+
+  const handleSendMessage = () => {
+    if (message.length > 0) {
+      sendDirectMessage({
+        receiverUserId: user.id,
+        content: message,
+      });
+      setInputMessage("");
+    }
+  };
+
   return (
     <div className="message">
       <div className="message__info">
@@ -21,8 +52,15 @@ function Message({ timestamp, message, user }) {
             {new Date(parseInt(timestamp)).getMinutes()}
           </span>
         </h5>
-        <p>{message}</p>
+        <InputEmoji
+          placeholder={`Write message to ${user.username}`}
+          value={message}
+          cleanOnEnter
+          onChange={setInputMessage}
+          onEnter={handleSendMessage}
+        />
       </div>
+      <div ref={messagesEndRef} />
     </div>
   );
 }
